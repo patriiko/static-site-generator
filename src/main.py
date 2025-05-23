c55d91bb0dd01a0c89d6f6e7079fae3dc88115c7
patriiko <ostrunicpersona@gmail.com>
2025-05-23 21:17:11 +0200

    initial commit: add textnode

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+def main():
+    # Testing
+    text = TextNode("Ovo je kod", TextType.LINK, "https://www.boot.dev")
+    text1 = TextNode("Ovo je kod", TextType.IMAGE, "https://www.boot.dev")
+    print(TextNode.__eq__(text, text1))
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, TextType, url=None):
+        self.text = text
+        self.text_type = TextType
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
