acd2073d43c3ee7933ad4c4100184754e225cefd
patriiko <ostrunicpersona@gmail.com>
2025-05-26 20:38:14 +0200

    add leafnode and unit tests for leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -32,6 +32,28 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(div, I like Boot.dev, children: None, {'href': 'boot.dev'})",
             htmlnode.__repr__()
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        expected_html = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), expected_html)
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_a_more_props(self):
+        node = LeafNode("a", "Click me!", {
+            "href": "https://www.google.com",
+            "class": "google",
+            "target": "_blank"
+        })
+        expected_html = '<a href="https://www.google.com" class="google" target="_blank">Click me!</a>'
+        self.assertEqual(node.to_html(), expected_html)
 
 if __name__ == "__main__":
     unittest.main()
