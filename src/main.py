d56c8ccb1119e96a7a8ee02801eb17d46e8945d5
patriiko <ostrunicpersona@gmail.com>
2025-07-06 20:35:42 +0200

    deploy website

--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
-from generate_content import generate_page
+from generate_content import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 if __name__ == "__main__":
     main()
