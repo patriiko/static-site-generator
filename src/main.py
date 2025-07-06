aa05c42e57ab9dfe97fdf0372ec0e3fe64f4e6c9
patriiko <ostrunicpersona@gmail.com>
2025-07-06 20:10:11 +0200

    add unit tests for generate content

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from generate_content import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -13,6 +15,12 @@ def main():
         shutil.rmtree(dir_path_public)
 
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 if __name__ == "__main__":
     main()
